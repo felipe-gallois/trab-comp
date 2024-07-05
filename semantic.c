67e7403648c774301353379eb520efa6aea4ebd1
Felipe Gallois <galloisfelipe@gmail.com>
2024-07-05 14:47:05 -0300

    Set literal datatypes

--- semantic.c
+++ semantic.c
@@ -10,9 +10,11 @@
 int semantic_errors = 0;
 
 int is_declaration(AstNode *node);
+int is_literal(AstNode *node);
 int is_redeclared(HashEntry *entry);
 void set_hash_type_from_decl_node(HashEntry *entry, AstNode *decl_node);
 void set_hash_datatype_from_type_node(HashEntry *entry, AstNode *type_node);
+void set_hash_datatype_from_type(HashEntry *entry);
 void print_redeclaration_error(char *identifier_name);
 void print_uncaught_parser_error();
 
@@ -34,6 +36,8 @@ void check_and_set_declarations(AstNode *node) {
             set_hash_type_from_decl_node(identifier_node->symbol, node);
             set_hash_datatype_from_type_node(identifier_node->symbol, type_node);
         }
+    } else if (is_literal(node)) {
+        set_hash_datatype_from_type(node->symbol);
     }
 
     for (int i = 0; i < MAX_CHILDREN; i++)
@@ -51,6 +55,20 @@ int is_declaration(AstNode *node) {
         || (node_type == AST_VEC_DECL)
         || (node_type == AST_VEC_DECL_DEF)
         || (node_type == AST_FUNC_DECL);
+}
+
+int is_literal(AstNode *node) {
+    if (node->type != AST_SYMBOL)
+        return 0;
+
+    int symbol_type = node->symbol->type;
+
+    return (symbol_type == SYMBOL_LIT_INT)
+        || (symbol_type == SYMBOL_LIT_CHAR)
+        || (symbol_type == SYMBOL_LIT_REAL)
+        || (symbol_type == SYMBOL_LIT_TRUE)
+        || (symbol_type == SYMBOL_LIT_FALSE)
+        || (symbol_type == SYMBOL_LIT_STRING);
 }
 
 int is_redeclared(HashEntry *entry) {
@@ -108,6 +126,31 @@ void set_hash_datatype_from_type_node(HashEntry *entry, AstNode *type_node) {
     }
 
     entry->datatype = datatype;
+}
+
+void set_hash_datatype_from_type(HashEntry *entry) {
+    switch (entry->type) {
+        case SYMBOL_LIT_INT:
+            entry->datatype = DATATYPE_INT;
+            break;
+        case SYMBOL_LIT_CHAR:
+            entry->datatype = DATATYPE_CHAR;
+            break;
+        case SYMBOL_LIT_REAL:
+            entry->datatype = DATATYPE_REAL;
+            break;
+        case SYMBOL_LIT_TRUE:
+            entry->datatype = DATATYPE_BOOL;
+            break;
+        case SYMBOL_LIT_FALSE:
+            entry->datatype = DATATYPE_BOOL;
+            break;
+        case SYMBOL_LIT_STRING:
+            entry->datatype = DATATYPE_STRING;
+            break;
+        default:
+            break;
+    }
 }
 
 void print_redeclaration_error(char *identifier_name) {
